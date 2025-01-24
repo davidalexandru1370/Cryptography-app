be21424ae247d8b4ddaa26cc4d167b677130254e
David Alexandru <fanefaraon696@gmail.com>
2025-01-24 22:36:07 +0200

    added: menu

---
+++ app/a.js
@@ -0,0 +1,13 @@
+foo();
+
+var foo;
+
+foo = function(){
+	console.log("foo");
+};
+
+function foo(){
+	console.log("bar");
+}
+
+foo();
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,52 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import { UdpServer } from "./pages/UdpServer";
 
 const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
+
+const Main: React.FC = () => {
+  const [selectedIndex, setSelectedIndex] = React.useState<number>(0);
+
+  const menuButtonStyle: React.CSSProperties = {
+    padding: "10px",
+    margin: "10px",
+    cursor: "pointer",
+    backgroundColor: "lightblue",
+    border: "none",
+    borderRadius: "5px",
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <div style={{ display: "flex" }}>
+        <button style={menuButtonStyle} onClick={() => setSelectedIndex(0)}>
+          Udp
+        </button>
+        <button style={menuButtonStyle} onClick={() => setSelectedIndex(1)}>
+          Time
+        </button>
+        <button style={menuButtonStyle} onClick={() => setSelectedIndex(2)}>
+          Manual MD5
+        </button>
+      </div>
+      <div style={{ flexGrow: 1 }}>
+        {selectedIndex === 0 && <UdpServer />}
+        {selectedIndex === 1 && <div>Time</div>}
+        {selectedIndex === 2 && <div>Manual MD5</div>}
+      </div>
+    </div>
+  );
+};
+
 root.render(
   <React.StrictMode>
-    <div>
-      <p>Hello davis</p>
-    </div>
+    <UdpServer />
   </React.StrictMode>
 );
