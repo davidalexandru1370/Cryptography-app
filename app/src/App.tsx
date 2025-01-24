e813782354a16dfeb4d5eacf1967aff3b417b7dc
David Alexandru <fanefaraon696@gmail.com>
2025-01-24 20:00:31 +0200

    added: time comparisons component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import { TimeComparisons } from "./pages/timeComparions";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <p>Hello davis</p>
+    <TimeComparisons />
   </React.StrictMode>
 );
